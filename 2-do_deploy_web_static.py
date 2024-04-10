8d31bf3be70e4faf0c1fed513739947320ee4a84
wanjocharles <wanjocharles98@gmail.com>
2024-04-10 09:51:13 -0700

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,46 +1,67 @@
 #!/usr/bin/python3
-""" a Fabric script (based on the file 1-pack_web_static.py) that distributes..
-    ..an archive to your web servers, using the function do_deploy: """
+"""
+A Fabric script that distributes the archive to the web
+servers using do_deploy function.
+"""
 
-
-from fabric.api import *
-from datetime import datetime
+from fabric.api import env, put, run
 from os.path import exists
 
+# Define the user and SSH key for accessing the server
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/my_key'
 
-env.hosts = ['35.237.166.125', '54.167.61.201']  # <IP web-01>, <IP web-02>
-# ^ All remote commands must be executed on your both web servers
-# (using env.hosts = ['<IP web-01>', 'IP web-02'] variable in your script)
+# Define the list of web servers
+env.hosts = ['100.25.0.107', '100.26.252.88']
 
 
 def do_deploy(archive_path):
-    """ distributes an archive to my web servers
-    """
-    if exists(archive_path) is False:
-        return False  # Returns False if the file at archive_path doesnt exist
-    filename = archive_path.split('/')[-1]
-    # so now filename is <web_static_2021041409349.tgz>
-    no_tgz = '/data/web_static/releases/' + "{}".format(filename.split('.')[0])
-    # curr = '/data/web_static/current'
-    tmp = "/tmp/" + filename
+    '''
+    Distributes an archive to the web servers.
+    Returns False if the file at the path archive_path doesn’t exist, else it
+    deploys it
+    '''
+    if not exists(archive_path):
+        return False
 
     try:
-        put(archive_path, "/tmp/")
-        # ^ Upload the archive to the /tmp/ directory of the web server
-        run("mkdir -p {}/".format(no_tgz))
-        # Uncompress the archive to the folder /data/web_static/releases/
-        # <archive filename without extension> on the web server
-        run("tar -xzf {} -C {}/".format(tmp, no_tgz))
-        run("rm {}".format(tmp))
-        run("mv {}/web_static/* {}/".format(no_tgz, no_tgz))
-        run("rm -rf {}/web_static".format(no_tgz))
-        # ^ Delete the archive from the web server
-        run("rm -rf /data/web_static/current")
-        # Delete the symbolic link /data/web_static/current from the web server
-        run("ln -s {}/ /data/web_static/current".format(no_tgz))
-        # Create a new the symbolic link /data/web_static/current on the
-        # web server, linked to the new version of your code
-        # (/data/web_static/releases/<archive filename without extension>)
+        # Upload the archive to the /tmp/ directory of the web server
+        put(archive_path, '/tmp/')
+
+        # Get the base name of the archive file
+        archive_filename = archive_path.split('/')[-1]
+        archive_name = archive_filename.split('.')[0]
+
+        # Create the folder for the new version on the web server
+        run('mkdir -p /data/web_static/releases/{}/'.format(archive_name))
+
+        # Uncompress the archive to the folder on the web server
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(
+            archive_filename, archive_name))
+
+        # Delete the archive from the web server
+        run('rm /tmp/{}'.format(archive_filename))
+
+        # Move the contents of the uncompressed folder to the version folder
+        my_command = (
+            'mv /data/web_static/releases/{}/web_static/*'
+            .format(archive_name))
+        my_command += ' /data/web_static/releases/{}/'.format(archive_name)
+        run(my_command)
+
+        # Remove the empty web_static folder
+        run('rm -rf /data/web_static/releases/{}/web_static'
+            .format(archive_name))
+
+        # Remove the current symbolic link
+        run('rm -rf /data/web_static/current')
+
+        # Create a new symbolic link to the new version
+        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
+            .format(archive_name))
+
+        print("New version deployed!")
         return True
-    except:
+
+    except Exception as e:
         return False
